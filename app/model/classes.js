b3b2398362d9d8242b6555e38593a2c88d9eaf2e
Tristan <spinnewyn.tristan@tutanota.com>
2021-03-02 11:42:12 +0100

    init front with model

---
+++ app/model/classes.js
@@ -0,0 +1,69 @@
+class User{
+    constructor(trigramme,firstname,lastname,email,roleUserId,token){
+        this.trigramme = trigramme
+        this.firstname = firstname
+        this.lastname = lastname
+        this.email = email
+        this.roleUserId = roleUserId
+        this.token =token
+    }
+}
+
+class Project{
+    constructor(nameProject,userId){
+        this.nameProject =nameProject
+        this.userId = userId
+    }
+}
+
+class TypeExigence{
+    constructor(nameExigence){
+        this.nameExigence = nameExigence
+    }
+}
+
+class Exigence{
+    constructor(descExigence,isFonctionnel,projectId,typeExigenceId){
+        this.descExigence =descExigence
+        this.isFonctionnel = isFonctionnel
+        this.projectId = projectId
+        this.typeExigenceId = typeExigenceId
+    }
+}
+
+class RoleUser{
+    constructor(nameRole){
+        this.nameRole = nameRole
+    }
+}
+
+class Jalon{
+    constructor(dateLivPrev,dateLivReel,jalonName,projectId,userId){
+        this.dateLivPrev = dateLivPrev
+        this.dateLivReel = dateLivReel
+        this.jalonName = jalonName
+        this.projectId = projectId
+        this.userId = userId
+    }
+}
+
+class Task{
+    constructor(nameTask,descTask,nbDays,dateStartTaskPrev,dateStartTaskReal,dateEndTask,jalonId,userId,taskIdDepend){
+        this.nameTask = nameTask
+        this.descTask = descTask
+        this.nbDays = nbDays
+        this.dateStartTaskPrev = dateStartTaskPrev
+        this.dateStartTaskReal = dateStartTaskReal
+        this.dateEndTask = dateEndTask
+        this.jalonId = jalonId
+        this.userId = userId
+        this.taskIdDepend = taskIdDepend
+    }
+}
+
+class TaskExigence{
+    constructor(taskId,exigenceId){
+        this.taskId = taskId
+        this.exigenceId = exigenceId
+    }
+}
---
+++ app/services/baseapiservice.js
@@ -0,0 +1,12 @@
+const sercieBaseUrl = "https://localhost:44365/api/"
+
+class BaseApiService{
+    constructor(url){
+        this.url = `${serviceBaseUrl}/${url}`
+        this.token = sessionStorage.getItem("token")
+        this.headers = new Headers()
+        if(this.token !== undefined){
+            this.headers.append("Authorization", `Bearer ${this.token}`)
+        }
+    }
+}
